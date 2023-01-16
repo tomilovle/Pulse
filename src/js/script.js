f10b3f5fa0a028ab2092bdc8be84d27df2826a8c
Lev <Lev_tomilov@mail.ru>
2023-01-16 13:54:11 +0500

    add carousel

---
+++ src/js/script.js
@@ -0,0 +1,17 @@
+ const slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    controls: false,
+    nav: false,
+    mouseDrag: true
+  });
+
+  document.querySelector('.prev').addEventListener('click', function () {
+    slider.goTo('prev');
+  });
+
+  document.querySelector('.next').addEventListener('click', function () {
+    slider.goTo('next');
+  });
