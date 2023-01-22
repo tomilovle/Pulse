de3282558dc85928c04488ccb665fe23677af288
Lev <Lev_tomilov@mail.ru>
2023-01-22 09:34:20 +0500

    add phone number mask

--- src/js/script.js
+++ src/js/script.js
@@ -91,6 +91,10 @@ $(document).ready(function(){
     validateForms('#consultation-form');
     validateForms('#consultation form');
     validateForms('#order form');
+
+    //mask
+
+    $('input[name=phone]').mask("+7 (999) 999-9999");
     
 }); 
 
