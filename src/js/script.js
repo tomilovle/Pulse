e53544b3981132deb04397217e09dbcc99c379dd
Lev <Lev_tomilov@mail.ru>
2023-01-23 14:51:11 +0500

    add phpmailer

--- src/js/script.js
+++ src/js/script.js
@@ -95,6 +95,22 @@ $(document).ready(function(){
     //mask
 
     $('input[name=phone]').mask("+7 (999) 999-9999");
+
+    $('form').submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+
+            $('form').trigger('reset');
+        });
+        return false;
+    });
     
 }); 
 
