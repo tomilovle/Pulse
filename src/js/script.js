16e623fbac61e6f25c7f06f35eb77198179c4b0c
Lev <Lev_tomilov@mail.ru>
2023-01-23 16:46:17 +0500

    add animations

--- src/js/script.js
+++ src/js/script.js
@@ -112,5 +112,15 @@ $(document).ready(function(){
         return false;
     });
     
+
+    const wow = new WOW({
+        boxClass: 'wow',
+        animateClass: 'animate__animated', 
+        offset: 30, 
+        mobile: true, 
+        live: true 
+      });
+      wow.init(); 
+      
 }); 
 
